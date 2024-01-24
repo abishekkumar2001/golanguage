579d73dbe4bf25d8fc45aa24d4cb1bf1b7b2b445
abishekkumar23 <abishekkumar.k@datasirpi.com>
2024-01-24 17:35:00 +0530

    TCP Socket Programming in Go

---
+++ tcp_socket_programming/client.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	
+	//Connecting to the TCP Server on localhost
+	conn,err := net.Dial("tcp","localhost:8080")
+	if err != nil {
+		fmt.Println("Error Connecting",err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	//Sending message to the server
+	msg := "Hello Server !"
+	_, err = conn.Write([]byte(msg))
+	if err != nil {
+		fmt.Println("Error Writing to server",err)
+		return
+	}
+	fmt.Printf("Message Sent is %s",msg)
+
+	//Echoed message from server
+	buffer := make([]byte,1024)
+	n,err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error Reading",err)
+		return
+	}
+	fmt.Printf("\nEchoed message is %s\n",buffer[:n])
+
+}
+
